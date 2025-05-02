59971731c376d878ad05356b9d5a43420dc47b4f
Udi Shamir <udishamir@gmail.com>
2025-05-02 09:17:15 -0700

    Adding process notification routine

--- cosmos_drv.cpp
+++ cosmos_drv.cpp
@@ -1,3 +1,7 @@
+/*
+	References: https://github.com/AdaCore/gsh/tree/master/os/src
+*/
+
 #include <ntddk.h>
 #include <sal.h>
 #include <ntstrsafe.h>
@@ -5,11 +9,37 @@
 // Must have forward declaration otherwise the compiler wont build.
 extern "C" void DriverUnload(_In_ PDRIVER_OBJECT DriverObject);
 
+VOID ProcessNotifyCallBack(
+	HANDLE ParentId,
+	HANDLE ProcessId,
+	BOOLEAN Create
+) {
+	if (Create) {
+		// Process being created
+		DbgPrint("Cosmos: Process Created - PID: %llu, Parent PID: %llu\n",
+			(ULONG64)ProcessId, (ULONG64)ParentId);
+	}
+	else {
+		// Process being terminated
+		DbgPrint("Cosmos: Process Terminated - PID: %llu\n", (ULONG64)ProcessId);
+	}
+}
+
 extern "C"
 NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 {
     UNREFERENCED_PARAMETER(RegistryPath);
 	//UNREFERENCED_PARAMETER(DriverObject);
+
+	NTSTATUS status = STATUS_SUCCESS;
+    
+    // Register the PsSetCreateProcessNotifyRoutineEx to point to my callback
+	NTSTATUS ProcessMon = PsSetCreateProcessNotifyRoutine(ProcessNotifyCallBack, FALSE);
+	if (ProcessMon != status)
+	{
+		DbgPrint("Cosmos: Failed to register process notification callback (0x%08X)\n", status);
+		return ProcessMon;
+	}
 
     // Set the driver unload routine
     DriverObject->DriverUnload = DriverUnload;
@@ -22,10 +52,19 @@ NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING Regi
 }
 
 extern "C"
-VOID DriverUnload(PDRIVER_OBJECT DriverObject)
+VOID DriverUnload(_In_ PDRIVER_OBJECT DriverObject)
 {
+//UNREFERENCED_PARAMETER(RegistryPath);
 	UNREFERENCED_PARAMETER(DriverObject);
+	NTSTATUS status = STATUS_SUCCESS;
 
-    DbgPrint("Cosmos: Driver Unloaded Successfully!\n");
+	NTSTATUS ProcessMon = PsSetCreateProcessNotifyRoutine(ProcessNotifyCallBack, TRUE);
+	if (ProcessMon != status) {
+		DbgPrint("Cosmos: Failed to unregister process notify callback\n");
+		return;
+	}
+	else {
+		DbgPrint("Cosmos: Driver Unloaded Successfully!\n");
+	}
 	DbgPrintEx(DPFLTR_IHVVIDEO_ID, DPFLTR_INFO_LEVEL, "Cosmos: Driver Unloaded Successfully!\n");
 }
